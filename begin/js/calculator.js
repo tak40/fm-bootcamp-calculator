e0e6584252d4fda73cc5cf03b3196f0d5de92f7b
TK <118142360+tak40@users.noreply.github.com>
2024-07-03 09:15:13 -0400

    Finalize calculator app implementation: added buffer reset logic, fixed operator handling, and improved display update

--- begin/js/calculator.js
+++ begin/js/calculator.js
@@ -1,32 +1,101 @@
 let runningTotal = 0;
 let buffer = "0";
-let previousOperator;
+let previousOperator = null;
+let shouldResetBuffer = false;
 
 const screen = document.querySelector(".screen");
 
 function buttonClick(value) {
-  if (isNaN(value)) {
+  if (isNaN(parseInt(value))) {
+    // this is not a number
     handleSymbol(value);
   } else {
+    // this is a number
     handleNumber(value);
+  }
+  screen.innerText = buffer;
+}
+
+function handleSymbol(value) {
+  switch (value) {
+    case "C":
+      buffer = "0";
+      runningTotal = 0;
+      previousOperator = null;
+      break;
+    case "=":
+      if (previousOperator === null) {
+        // need two numbers to do math
+        return;
+      }
+      flushOperation(parseFloat(buffer));
+      previousOperator = null;
+      buffer = "" + runningTotal;
+      runningTotal = 0;
+      break;
+    case "←":
+      if (buffer.length === 1) {
+        buffer = "0";
+      } else {
+        buffer = buffer.substring(0, buffer.length - 1);
+      }
+      break;
+    case "+":
+    case "−":
+    case "×":
+    case "÷":
+      handleMath(value);
+      break;
   }
 }
 
-function handleSymbol(symbol) {}
-function handleNumber(numberString) {
+function handleMath(value) {
+  console.log("handleMath", value);
   if (buffer === "0") {
-    buffer = numberString;
-  } else {
-    buffer += numberString;
+    // do nothing
+    return;
   }
-  screen.innerText = buffer;
+
+  const intBuffer = parseFloat(buffer);
+
+  if (runningTotal === 0) {
+    runningTotal = intBuffer;
+  } else {
+    flushOperation(intBuffer);
+  }
+  previousOperator = value;
+  shouldResetBuffer = true;
+}
+
+function flushOperation(intBuffer) {
+  if (previousOperator === "+") {
+    runningTotal += intBuffer;
+  } else if (previousOperator === "−") {
+    runningTotal -= intBuffer;
+  } else if (previousOperator === "×") {
+    runningTotal *= intBuffer;
+  } else if (previousOperator === "÷") {
+    runningTotal /= intBuffer;
+  }
+}
+
+function handleNumber(numberString) {
+  if (shouldResetBuffer) {
+    buffer = numberString;
+    shouldResetBuffer = false;
+  } else {
+    if (buffer === "0") {
+      buffer = numberString;
+    } else {
+      buffer += numberString;
+    }
+  }
 }
 
 function init() {
   document
     .querySelector(".calc-buttons")
     .addEventListener("click", function (event) {
-      console.log(event);
       buttonClick(event.target.innerText);
     });
 }
